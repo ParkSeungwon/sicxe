8a984d2d3dc99bbd00ef4e01de08c27b9e17d8a1
zezeon <zezeon@msn.com>
2016-11-13 13:24:50 +0900

    clean garbage

--- 1.s
+++ 1.s
@@ -2,25 +2,25 @@ macro save store,three
  stx store
  ldx three
  sta store,x
- jsub 20
- jsub 20
- jsub 20
+ jsub 2
+ jsub 2
+ jsub 2
  stl store,x
  ldx store
 mend
 macro load store,three
  ldx three
  lda store,x
- jsub 20
- jsub 20
- jsub 20
+ jsub 2
+ jsub 2
+ jsub 2
  ldl store,x
  ldx store
 mend
  start 1000
 loop ldch string,x
  wd string
- jsub 20 //x++
+ jsub 2 //x++
  comp end_string
  save garage,three
  jeq quit
